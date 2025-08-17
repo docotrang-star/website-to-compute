f5f8db0ae8bb311b4a8a25c6fd3b5cc411f8b1e9
docotrang-star <docotrang@gmail.com>
2025-08-18 02:22:17 +0700

    Update script.js

--- public/script.js
+++ public/script.js
@@ -5,3 +5,11 @@
 // Print a message in the browser's dev tools console each time the page loads
 // Use your menus or right-click / control-click and choose "Inspect" > "Console"
 console.log("Hello 🌎");
+// Tạo một thông báo khi người dùng click vào link Shopee
+const reviewLinks = document.querySelectorAll('.review-item a');
+
+reviewLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        alert('Cảm ơn bạn đã ghé thăm!');
+    });
+});
